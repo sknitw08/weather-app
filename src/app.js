bfe020bd3fdd91fdd3da65f659e45743f4be04e9
Sanjay Kumar <sank@microsoft.com>
2019-03-12 18:24:02 +0530

    Set up for Heroku

--- src/app.js
+++ src/app.js
@@ -6,6 +6,7 @@ const geocode = require('./utils/geocode');
 const forecast = require('./utils/forecast');
 
 const app = express();
+const port = process.env.PORT || 3000;
 
 const viewsPath = path.join(__dirname, "..", "templates", "views");
 const partialsPath = path.join(__dirname, "..", "templates", "partials");
@@ -52,4 +53,4 @@ app.get('*', (req, res) => {
     })
 })
 
-app.listen(3000);
+app.listen(port);
